cc6af56b2d1eacfa21d7b2c3cc9c4a7953c4b51c
sgardziewicz-gd <sgardziewicz@griddynamics.com>
2022-06-15 14:41:40 +0200

    3 tasks done

--- practice/6_web_scraping/stock_info.py
+++ practice/6_web_scraping/stock_info.py
@@ -31,7 +31,6 @@ Links:
     - beautiful soup docs: https://www.crummy.com/software/BeautifulSoup/bs4/doc/
     - lxml docs: https://lxml.de/
 """
-
 import requests
 import pycountry
 from bs4 import BeautifulSoup
@@ -45,9 +44,7 @@ def get_symbols():
     i = 0
     for tr in soup.find_all('a', {'class': 'Fw(600) C($linkColor)'}):
         symbols[tr.text] = None
-        # i += 1
-        # if i > 5:
-        #     break
+
     return symbols
 
 
@@ -58,11 +55,8 @@ def findCountry(stringText):
             return country
     return None
 
-def get_info_from_profile(symbol):
+def get_info_from_profile(symbol, headers, cookies):
     url = "https://finance.yahoo.com/quote/{}/profile?p={}".format(symbol, symbol)
-    user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.115 Safari/537.36'
-    headers = {'User-Agent': user_agent}
-    cookies = {"cookie":"t=1655116572&j=1&u=1---&v=39"}
     response = requests.get(url, headers=headers, cookies=cookies)
     soup = BeautifulSoup(response.text, 'html.parser')
     profile_data = {
@@ -71,22 +65,65 @@ def get_info_from_profile(symbol):
         'country' : findCountry(soup.find_all('p', {'class': 'D(ib)'})[0].text),
         'employees' : soup.find_all('span', {'class': 'Fw(600)'})[2].text,
         'ceo_name' : soup.find_all('td', {'class': 'Ta(start)'})[0].text,
-        'ceo_year_born' : '1900' if soup.find_all('td', {'class': 'Ta(end)'})[2].text == 'N/A'\
-                                 else soup.find_all('td', {'class': 'Ta(end)'})[2].text
+        'ceo_year_born' : soup.find_all('td', {'class': 'Ta(end)'})[2].text 
     }
+    try:
+        profile_data['company_name'] = soup.find('h3', {'class': 'Fz(m)'}).text
+    except AttributeError:
+        profile_data['company_name'] = "N/A"
+
     return profile_data
 
 
-def fill_symbols_with_info(symbols):
+def get_info_from_statistics(symbol, headers, cookies):
+    url = "https://finance.yahoo.com/quote/{}/key-statistics?p={}".format(symbol, symbol)
+    response = requests.get(url, headers=headers, cookies=cookies)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    statistics_data = {
+        'code' : symbol,
+        '52_week_change' : soup.find_all('td', {'class':'Fw(500)'})[10].text
+    }
+    try:
+        statistics_data['company_name'] = soup.find('h1', {'class':'Fz(18px)'}).text.rsplit(' ', 1)[0]
+    except AttributeError:
+        statistics_data['company_name'] = 'N/A'
+
+    try:
+        statistics_data['total_cash'] = soup.find_all('td', {'class':'Ta(end)'})[52].text
+    except IndexError:
+        statistics_data['total_cash'] = 'N/A'
+
+    return statistics_data
+
+def fill_symbols_with_statistics_info(symbols, headers, cookies):
     for symbol in symbols:
-        symbols[symbol] = get_info_from_profile(symbol)
+        symbols[symbol] = get_info_from_statistics(symbol, headers, cookies)
     return symbols
 
 
-def get_n_youngest_companies(n, symbols):
-    symbols_sorted = sorted(symbols.items(), key=lambda x: x[1]['ceo_year_born'], reverse=True)
+def fill_symbols_with_profile_info(symbols, headers, cookies):
+    for symbol in symbols:
+        symbols[symbol] = get_info_from_profile(symbol, headers, cookies)
+    return symbols
+
+
+def get_n_best_52_week_change_companies(n, symbols):
+    symbols_sorted = sorted(symbols.items(), key=lambda x: float(x[1]['52_week_change'][:-1]) if x[1]['52_week_change'] != 'N/A' \
+                                                                                              else -100.00, reverse=True)
     return symbols_sorted[:n]
 
+def get_n_youngest_companies(n, symbols):
+    symbols_sorted = sorted(symbols.items(), key=lambda x: int(x[1]['ceo_year_born']) if x[1]['ceo_year_born'] != 'N/A' \
+                                                                                      else 0, reverse=True)
+    return symbols_sorted[:n]
+
+def append_holders(data_to_print, holders):
+    for holder in holders.items():
+        data_to_print.append([holder[1]['company_name'], holder[1]['shares'], holder[1]['date_reported'], holder[1]['out'], holder[1]['value']])
+
+def append_best_52_week_change_data(data_to_print, best_n):
+    for company in best_n:
+        data_to_print.append([company[1]['company_name'], company[1]['code'], company[1]['52_week_change'], company[1]['total_cash']])
 
 def append_youngest_ceos_data(data_to_print, youngest_5):
     for company in youngest_5:
@@ -94,7 +131,8 @@ def append_youngest_ceos_data(data_to_print, youngest_5):
                             company[1]['employees'], company[1]['ceo_name'], company[1]['ceo_year_born']])
 
 
-def print_sheet(column_names, title, padding_spaces, stock_info, append_function):
+def print_sheet(column_names, title, stock_info, append_function):
+    padding_spaces = (len(column_names) * 3) + 1
     data_to_print = []
     data_to_print.append(column_names)
     append_function(data_to_print, stock_info)
@@ -108,19 +146,56 @@ def print_sheet(column_names, title, padding_spaces, stock_info, append_function
     print(separator)
     for row in data_to_print:
         print ("| " + " | ".join((val.ljust(width) for val, width in zip(row, widths))) + " |")
-    print('\n')
+
+    print()
+
+
+def get_holders_data(headers, cookies):
+    url = "https://finance.yahoo.com/quote/BLK/holders?p=BLK"
+    response = requests.get(url, headers=headers, cookies=cookies)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    holders = {}
+
+    for tr in soup.find_all('td', {'class': 'Ta(start)'})[4:14]:
+        holders[tr.text] = None
+    details = []
+    for tr in soup.find_all('td', {'class': 'Ta(end)'})[0:40]:
+        details.append(tr.text)
+
+    shares = details[0::4]
+    date_reported = details[1::4]
+    out = details[2::4]
+    value = details[3::4]
+
+    for i, holder in enumerate(holders):
+        holders[holder] = {'company_name':holder, 'shares':shares[i], 'date_reported':date_reported[i], 'out':out[i], 'value':value[i]}
+
+    return holders
 
 
 def main():
+    user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.115 Safari/537.36'
+    headers = {'User-Agent': user_agent}
+    cookies = {"cookie":"t=1655116572&j=1&u=1---&v=39"}
+
     symbols = get_symbols()
-    symbols = fill_symbols_with_info(symbols)
-    youngest_5 = get_n_youngest_companies(5, symbols)
 
-    column_names = (['Name', 'Code', 'Country', 'Employees', 'CEO Name', 'CEO Year Born'])
-    title = '5 stocks with most youngest CEOs'
-    padding_spaces = 19
+    symbols_with_profile_info = fill_symbols_with_profile_info(symbols, headers, cookies)
+    youngest_5 = get_n_youngest_companies(5, symbols_with_profile_info)
+    column_names_youngest = (['Name', 'Code', 'Country', 'Employees', 'CEO Name', 'CEO Year Born'])
+    title_youngest = '5 stocks with most youngest CEOs'
+    print_sheet(column_names_youngest, title_youngest, youngest_5, append_youngest_ceos_data)
 
-    print_sheet(column_names, title, padding_spaces, youngest_5, append_youngest_ceos_data)
+    symbols_with_statistics = fill_symbols_with_statistics_info(symbols, headers, cookies)
+    best_10 = get_n_best_52_week_change_companies(10, symbols_with_statistics)
+    column_names_best_52_week = (['Name', 'Code', '52-Week Change', 'Total Cash'])
+    title_best_52_week = '10 stocks with best 52-Week change'
+    print_sheet(column_names_best_52_week, title_best_52_week, best_10, append_best_52_week_change_data)
+
+    column_names_holders = (['Name', 'Shares', 'Date Reported', '% Out', 'Value'])
+    title_holders = '10 largest holders of Blackrock Inc.'
+    holders = get_holders_data(headers, cookies)
+    print_sheet(column_names_holders, title_holders, holders, append_holders)
 
 
 if __name__ == '__main__':
