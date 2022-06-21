f2701d4d7911c7f13ca9acf4533c4d101a6394db
sgardziewicz-gd <sgardziewicz@griddynamics.com>
2022-06-21 19:53:35 +0200

    task1 done

--- practice/7_concurrency/task1_fibonacci/template.py
+++ practice/7_concurrency/task1_fibonacci/template.py
@@ -1,5 +1,10 @@
+import multiprocessing
 import os
 from random import randint
+from multiprocessing import Pool
+import time
+import csv
+import concurrent.futures
 
 
 OUTPUT_DIR = './output'
@@ -16,16 +21,52 @@ def fib(n: int):
 
 
 def func1(array: list):
-    pass
+    with Pool(multiprocessing.cpu_count()) as p:
+        p.map(calc, array)
 
+def calc(number: int):
+    with open(f'{OUTPUT_DIR}/{number}.txt', 'w') as f:
+        f.write(str(fib(number)))
+
+def read(filename: str):
+    path = f'{OUTPUT_DIR}/{filename}'
+    with open(path, 'r') as f:
+        num = f.readline()
+    return [filename.strip('.txt'), num]
 
 def func2(result_file: str):
-    pass
+    files = os.listdir(OUTPUT_DIR)
+    data = []
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=16) as executor:
+        futures = {executor.submit(read, file) for file in files}
+        
+        for fut in concurrent.futures.as_completed(futures):
+            data.append(fut.result())
+
+    write_csv(data, result_file)
+
+
+def write_csv(data, path):
+    with open(path, 'w+') as f:
+        write = csv.writer(f)
+        write.writerows(reversed(data))
 
 
 if __name__ == '__main__':
     if not os.path.exists(OUTPUT_DIR):
         os.makedirs(OUTPUT_DIR)
-
-    func1(array=[randint(1000, 100000) for _ in range(1000)])
+    func1([randint(1000, 100000) for _ in range(500)])
+    start_time = time.time()
     func2(result_file=RESULT_FILE)
+    print(" --- %s seconds" % (time.time() - start_time))
+
+#--func2
+#for each                         ---- 0.75
+#256 workers threadpool range 500  --- 0.4
+#64 workers threadpool range 500  --- 0.4
+#16 workers threadpool range 500    --- 0.37
+#8 workers threadpool range 500    --- 0.39
+
+#8 cpu processing 500             ---- 0.72
+
