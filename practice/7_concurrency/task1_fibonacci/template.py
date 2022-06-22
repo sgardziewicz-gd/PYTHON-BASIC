39e0ba300d8743683025c720c35e7e9e000a6958
sgardziewicz-gd <sgardziewicz@griddynamics.com>
2022-06-22 15:36:53 +0200

    task2 done

--- practice/7_concurrency/task1_fibonacci/template.py
+++ practice/7_concurrency/task1_fibonacci/template.py
@@ -2,10 +2,8 @@ import multiprocessing
 import os
 from random import randint
 from multiprocessing import Pool
-import time
 import csv
 import concurrent.futures
-
 
 OUTPUT_DIR = './output'
 RESULT_FILE = './output/result.csv'
@@ -15,7 +13,7 @@ def fib(n: int):
     """Calculate a value in the Fibonacci sequence by ordinal number"""
 
     f0, f1 = 0, 1
-    for _ in range(n-1):
+    for _ in range(n - 1):
         f0, f1 = f1, f0 + f1
     return f1
 
@@ -24,9 +22,11 @@ def func1(array: list):
     with Pool(multiprocessing.cpu_count()) as p:
         p.map(calc, array)
 
+
 def calc(number: int):
     with open(f'{OUTPUT_DIR}/{number}.txt', 'w') as f:
         f.write(str(fib(number)))
+
 
 def read(filename: str):
     path = f'{OUTPUT_DIR}/{filename}'
@@ -34,13 +34,14 @@ def read(filename: str):
         num = f.readline()
     return [filename.strip('.txt'), num]
 
+
 def func2(result_file: str):
     files = os.listdir(OUTPUT_DIR)
     data = []
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=16) as executor:
         futures = {executor.submit(read, file) for file in files}
-        
+
         for fut in concurrent.futures.as_completed(futures):
             data.append(fut.result())
 
@@ -57,16 +58,7 @@ if __name__ == '__main__':
     if not os.path.exists(OUTPUT_DIR):
         os.makedirs(OUTPUT_DIR)
     func1([randint(1000, 100000) for _ in range(500)])
-    start_time = time.time()
     func2(result_file=RESULT_FILE)
-    print(" --- %s seconds" % (time.time() - start_time))
 
-#--func2
-#for each                         ---- 0.75
-#256 workers threadpool range 500  --- 0.4
-#64 workers threadpool range 500  --- 0.4
-#16 workers threadpool range 500    --- 0.37
-#8 workers threadpool range 500    --- 0.39
 
-#8 cpu processing 500             ---- 0.72
 
